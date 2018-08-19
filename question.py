80122928753329381343c33db52723254f7add4b
Roj <joa.dev@live.com>
2018-08-19 00:37:55 -0300

    New criteria for the dispersion of scores
    We calculate the minimum difference between any two scores for a
    text. If that is lower than a preset value, then we accept the
    scores and take the text out of the circulation.
    I wanted to use standard deviation but it is too sensitive to
    outliers.

--- question.py
+++ question.py
@@ -6,6 +6,8 @@ from model import *
 
 # TODO: put in a .yml file
 NUM_QUESTIONS = 3
+MIN_QUESTIONS = 2 # Integrity constraint for Text.completed
+MAX_DIFF = 0.4 # between any two elements (invariant to outliers)
 
 def get_questions():
     """ Returns a number of questions for the users to tag.
@@ -26,6 +28,17 @@ def get_js(app_uuid):
     # TODO: implement actual js.
     return "\n".join(str(text.uuid) + " - " + text.text for text in questions)
 
+def min_diff(vector):
+    """ Returns the minimum difference between any two elements in a list.
+        Runs in O(n log n)
+    """
+    vector = sorted(vector)
+
+    min_diff = vector[1]-vector[0]
+    for i in range(1, len(vector) - 1):
+        min_diff = min(min_diff, vector[i+1] - vector[i])
+
+    return min_diff
 
 def create_tags(app_uuid, user_id, tags):
     """ Creates the tags in the database.
@@ -45,8 +58,15 @@ def create_tags(app_uuid, user_id, tags):
                    user_id=user_id,
                    text_uuid=tag["text_uuid"],
                    score=tag["tag"])
-        if len(list(Tag.select().where(Tag.text_uuid == tag["text_uuid"]))) > 2:
-            Text.update(completed=True).where(Text.uuid == tag["text_uuid"]).execute()
+        all_tags = list(Tag.select().where(Tag.text_uuid == tag["text_uuid"]))
+        if len(all_tags) >= MIN_QUESTIONS:
+            all_scores = [t.score for t in all_tags]
+            # It is tempting to use standard deviation, but it is not sensitive
+            # to outliers. e.g. stdev([1, -0.5, -0.6]) ~= 0.9
+            if min_diff(all_scores) <= MAX_DIFF:
+                (Text.update(completed=True)
+                    .where(Text.uuid == tag["text_uuid"])
+                    .execute())
 
     return True
 
